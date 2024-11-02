7ff976df156bf06150f91d8559d47eb9a887b194
AsyncX <1158271328@qq.com>
2024-11-02 12:23:20 -0400

    split HTML and controller

--- assign1p2/app/controllers/YouTubeController.java
+++ assign1p2/app/controllers/YouTubeController.java
@@ -4,12 +4,12 @@ import play.mvc.Controller;
 import play.mvc.Result;
 import services.YouTubeService;
 import com.google.api.services.youtube.model.SearchResult;
+import play.twirl.api.Html;
 
 import javax.inject.Inject;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class YouTubeController extends Controller {
 
@@ -23,31 +23,8 @@ public class YouTubeController extends Controller {
     public Result search(String query) {
         try {
             List<SearchResult> results = youtubeService.searchVideos(query);
-
-            // Convert results to HTML format for display
-            String output = results.stream()
-                    .map(result -> {
-                        String videoUrl = "https://www.youtube.com/watch?v=" + result.getId().getVideoId();
-                        String channelUrl = "https://www.youtube.com/channel/" + result.getSnippet().getChannelId();
-                        return String.format(
-                                "<div style='margin-bottom: 20px;'>"
-                                        + "<h3><a href='%s' target='_blank'>%s</a></h3>"  // Video title and link
-                                        + "<p>Channel: <a href='%s' target='_blank'>%s</a></p>"  // Channel title and link
-                                        + "<p>%s</p>"  // Video description
-                                        + "<img src='%s' alt='Thumbnail' style='max-width: 200px; height: auto;'/>"  // Thumbnail
-                                        + "</div>",
-                                videoUrl,
-                                result.getSnippet().getTitle(),
-                                channelUrl,
-                                result.getSnippet().getChannelTitle(),
-                                result.getSnippet().getDescription(),
-                                result.getSnippet().getThumbnails().getDefault().getUrl()
-                        );
-                    })
-                    .collect(Collectors.joining("<hr/>"));  // Separate each video with a horizontal line
-
-            // Return response with HTML content
-            return ok("<html><body>" + output + "</body></html>").as("text/html");
+            Html html = views.html.youtubeResults.render(results);
+            return ok(html).as("text/html");
         } catch (IOException e) {
             e.printStackTrace();
             return internalServerError("Error fetching data from YouTube API");
