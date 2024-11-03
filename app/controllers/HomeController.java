111143b442088b5fd22e35099002e7d77644fd83
Dorreen Rostami <dor.rostami@gmail.com>
2024-11-03 15:46:02 -0500

    added redirectController and VideoInfo
    redirect from / to /ytlytics

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,20 +1,25 @@
 package controllers;
 
+import models.VideoInfo;
 import play.mvc.Controller;
 import play.mvc.Result;
+import scala.jdk.javaapi.CollectionConverters;
 import views.html.hello;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeController extends Controller {
-
+    private static List<VideoInfo> allSearchResults = new ArrayList<>();
 
     // Clear search results and display homepage
     public Result hello() {
-        return ok(hello.render(""));
+        allSearchResults.clear();
+        return ok(hello.render("", CollectionConverters.asScala(allSearchResults).toList()));
     }
 
-    // Search function, get the results of a new search and save them
+    // Get the results of a new search and save them
     public Result search(String query) {
-        return ok(hello.render(""));
+        return ok(hello.render("", CollectionConverters.asScala(allSearchResults).toList()));
     }
 }
