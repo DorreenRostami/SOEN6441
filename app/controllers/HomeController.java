f23125c115f59df2d01057c6302cfd4efbae55d9
Dorreen Rostami <dor.rostami@gmail.com>
2024-11-03 18:18:18 -0500

    getting videos from youtubeService

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -3,8 +3,8 @@ package controllers;
 import models.VideoInfo;
 import play.mvc.Controller;
 import play.mvc.Result;
-import scala.jdk.javaapi.CollectionConverters;
 import views.html.hello;
+import com.google.api.services.youtube.model.SearchResult;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,11 +26,28 @@ public class HomeController extends Controller {
     // Clear search results and display homepage
     public Result hello() {
         allSearchResults.clear();
-        return ok(hello.render("", CollectionConverters.asScala(allSearchResults).toList()));
+        return ok(hello.render("", allSearchResults));
     }
 
     // Get the results of a new search and save them
     public Result search(String query) {
-        return ok(hello.render("", CollectionConverters.asScala(allSearchResults).toList()));
+        List<SearchResult> results;
+        try {
+            results = youtubeService.searchVideos(query);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        allSearchResults = results.stream().map(result -> new VideoInfo(
+                result.getSnippet().getTitle(),
+                "https://www.youtube.com/watch?v=" + result.getId().getVideoId(),
+                result.getSnippet().getChannelTitle(),
+                "https://www.youtube.com/channel/" + result.getSnippet().getChannelId(),
+                result.getSnippet().getThumbnails().getDefault().getUrl(),
+                result.getSnippet().getDescription()
+        )).toList();
+
+        return ok(hello.render("", allSearchResults));
     }
 }
+
