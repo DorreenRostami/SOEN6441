ca4d19718fc426510a633cc7803f4db6d8a84071
Dorreen Rostami <dor.rostami@gmail.com>
2024-12-01 15:53:45 -0500

    clean search history class

--- app/models/SearchHistory.java
+++ app/models/SearchHistory.java
@@ -1,10 +1,7 @@
 package models;
 
-import com.google.api.services.youtube.model.SearchResult;
 import services.SentimentAnalyzer;
 
-import java.io.IOException;
-import java.util.stream.Collectors;
 import java.util.List;
 
 /**
@@ -47,44 +44,6 @@ public class SearchHistory {
         return results;
     }
 
-
-    /**
-     * Append the results found from the YouTube API to the search history (which includes the 10 most
-     * recent queries and 10 videos for each query, so 100 videos in total)
-     *
-     * @param searchHistoryList The current list of search history to which the new entry will be added
-     * @param query The search query
-     * @param results A list of YouTube SearchResult objects containing video details
-     * @return The updated searchHistoryList containing the new entries
-     * @author Dorreen Rostami - implementation
-     *
-     * @author Hao - changed channelURL so that clicking on it opens a web page containing all available profile
-     * information about a channel instead of opening the channel in YouTube
-     *
-     * @author Hamza Asghar Khan - Updated the video description to get the full description and not a snippet
-     */
-    public static List<SearchHistory> addToSearchHistory(List<SearchHistory> searchHistoryList, String query, List<SearchResult> results, Cache cache){
-        List<VideoInfo> videoInfoList = results.stream().map(result -> {
-            String videoId = result.getId().getVideoId();
-
-            //get full description instead of a snippet of it
-            String description = "";
-            try {
-                description = cache.getDescription(videoId);
-            } catch (IOException e){
-                System.out.println("Unable to fetch description for videoId: " + videoId);
-            }
-            return new VideoInfo(result, description);
-        }).collect(Collectors.toList());
-
-        // limit to 10 most recent search histories
-        if (searchHistoryList.size() > 10) {
-            searchHistoryList = searchHistoryList.subList(0, 10);
-        }
-
-        return searchHistoryList;
-    }
-
     /**
      * Generates an HTML string for displaying search results (and the query title if on results page)
      * @param showQuery a boolean indicating whether to display the query title and sentiment
@@ -104,5 +63,4 @@ public class SearchHistory {
         html.append("<div class=\"separator\"></div>");
         return html.toString();
     }
-
 }
--- test/models/SearchHistoryTest.java
+++ test/models/SearchHistoryTest.java
@@ -1,30 +1,20 @@
 package models;
 
-import com.google.api.services.youtube.model.*;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import services.SentimentAnalyzer;
-import util.TestHelper;
 
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Unit tests for the {@link SearchHistory} class
  * @author Dorreen & Hao
  */
 public class SearchHistoryTest {
-
-    @Mock
-    private Cache cache;
-
 
     @Mock
     private SentimentAnalyzer sentimentAnalyzer;
@@ -34,87 +24,6 @@ public class SearchHistoryTest {
         MockitoAnnotations.openMocks(this);
     }
 
-
-    /**
-     * Test case for the {@link SearchHistory#addToSearchHistory} method.
-     * Check that the search history is correctly updated when new search results (a list of 10 results) are added
-     * @author Dorreen Rostami
-     */
-    @Test
-    public void testAddToSearchHistory() throws IOException {
-        List<SearchHistory> searchHistoryList = new ArrayList<>();
-        String query = "query";
-        String cachedDescription = "Cached description";
-        String videoId = "v1";
-        when(cache.getDescription(videoId)).thenReturn(cachedDescription);
-
-        SearchResult res = TestHelper.createMockSearchResult(videoId, "Title", "Channel", "c1", "https://thumbnail/1", cachedDescription);
-        List<SearchResult> results = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            results.add(res);
-        }
-
-        List<SearchHistory> updatedSearchHistory = SearchHistory.addToSearchHistory(searchHistoryList, query, results, cache);
-
-        assertEquals(1, updatedSearchHistory.size());
-        assertEquals(query, updatedSearchHistory.get(0).getQuery());
-        assertEquals(10, updatedSearchHistory.get(0).getResults().size());
-
-        VideoInfo firstResult = updatedSearchHistory.get(0).getResults().get(0);
-        assertEquals("Title", firstResult.getVideoTitle());
-        assertEquals("https://www.youtube.com/watch?v=v1", firstResult.getVideoUrl());
-        assertEquals("Channel", firstResult.getChannelTitle());
-        assertEquals("channel?query=c1", firstResult.getChannelUrl());  // updated channelURL(it's false before)
-        assertEquals("https://thumbnail/1", firstResult.getThumbnailUrl());
-        assertEquals(cachedDescription, firstResult.getDescription());
-        assertEquals("/video?videoId=v1", firstResult.getTagsUrl());
-        verify(cache, times(10)).getDescription(videoId);
-    }
-
-    /**
-     * Tests that no more than 10 search history objects are kept in the search history list when the
-     * {@link SearchHistory#addToSearchHistory} method is called
-     * @author Dorreen Rostami
-     */
-    @Test
-    public void testAddToSearchHistory_keep10() throws IOException {
-        String cachedDescription = "Cached description";
-        String videoId = "v1";
-        when(cache.getDescription(videoId)).thenReturn(cachedDescription);
-        SearchResult res = TestHelper.createMockSearchResult(videoId, "Title", "Channel", "c1", "https://thumbnail/1", cachedDescription);
-        List<SearchResult> results = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            results.add(res);
-        }
-
-        List<SearchHistory> searchHistoryList = new ArrayList<>();
-        for (int i = 0; i < 11; i++) {
-            searchHistoryList = SearchHistory.addToSearchHistory(searchHistoryList, "query", results, cache);
-        }
-
-        assertEquals(10, searchHistoryList.size());
-    }
-
-    /**
-     * Tests that the description becomes an empty string when the cache throws an exception in the
-     * {@link SearchHistory#addToSearchHistory} method
-     * @author Dorreen Rostami
-     */
-    @Test
-    public void testAddToSearchHistory_throwException() throws IOException {
-        String videoId = "v1";
-        when(cache.getDescription(videoId)).thenThrow(new IOException("Mocked IOException"));
-        SearchResult res = TestHelper.createMockSearchResult(videoId, "Title", "Channel", "c1", "https://thumbnail/1", "desc");
-        List<SearchResult> results = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            results.add(res);
-        }
-        List<SearchHistory> searchHistoryList = SearchHistory.addToSearchHistory(new ArrayList<>(), "query", results, cache);;
-
-        VideoInfo firstResult = searchHistoryList.get(0).getResults().get(0);
-        assertEquals("", firstResult.getDescription());
-        assertEquals(1, searchHistoryList.size());
-    }
 
     /**
      * Tests that the getters and setters of the {@link SearchHistory} class
